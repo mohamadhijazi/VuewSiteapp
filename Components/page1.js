0c598bed99f9b14c90bbc9746255504c4bc66fd8
MHijazi <mohamadhijazi91@gmail.com>
2024-04-21 19:02:48 +0300

    css

--- Components/page1.js
+++ Components/page1.js
@@ -59,6 +59,7 @@ vueapp.component("BodyComponent", {
       gender: "M",
       inTeam: true,
       desc: "TEchnology innovation Designer makers",
+      imgUrl:"../Innovation_files/images/image.jpg"
     });
     this.users.push({
       Id: 1,
@@ -66,6 +67,7 @@ vueapp.component("BodyComponent", {
       gender: "M",
       inTeam: true,
       desc: "TEchnology innovation Designer makers",
+      imgUrl:"../Innovation_files/images/image.jpg"
     });
     this.users.push({
       Id: 1,
@@ -73,6 +75,7 @@ vueapp.component("BodyComponent", {
       gender: "M",
       inTeam: true,
       desc: "TEchnology innovation Designer makers",
+      imgUrl:"../Innovation_files/images/image.jpg"
     });
     this.users.push({
       Id: 1,
@@ -80,20 +83,87 @@ vueapp.component("BodyComponent", {
       gender: "M",
       inTeam: true,
       desc: "TEchnology innovation Designer makers",
+      imgUrl:"../Innovation_files/images/image.jpg"
+    });
+     this.users.push({
+      Id: 1,
+      Name: "MohammadHijazi",
+      gender: "M",
+      inTeam: true,
+      desc: "TEchnology innovation Designer makers",
+      imgUrl:"../Innovation_files/images/image.jpg"
+    });
+     this.users.push({
+      Id: 1,
+      Name: "MohammadHijazi",
+      gender: "M",
+      inTeam: true,
+      desc: "TEchnology innovation Designer makers",
+      imgUrl:"../Innovation_files/images/image.jpg"
+    });
+     this.users.push({
+      Id: 1,
+      Name: "MohammadHijazi",
+      gender: "M",
+      inTeam: true,
+      desc: "TEchnology innovation Designer makers",
+      imgUrl:"../Innovation_files/images/image.jpg"
+    });
+     this.users.push({
+      Id: 1,
+      Name: "MohammadHijazi",
+      gender: "M",
+      inTeam: true,
+      desc: "TEchnology innovation Designer makers",
+      imgUrl:"../Innovation_files/images/image.jpg"
+    });
+     this.users.push({
+      Id: 1,
+      Name: "MohammadHijazi",
+      gender: "M",
+      inTeam: true,
+      desc: "TEchnology innovation Designer makers",
+      imgUrl:"../Innovation_files/images/image.jpg"
+    });
+     this.users.push({
+      Id: 1,
+      Name: "MohammadHijazi",
+      gender: "M",
+      inTeam: true,
+      desc: "TEchnology innovation Designer makers",
+      imgUrl:"../Innovation_files/images/image.jpg"
+    });
+     this.users.push({
+      Id: 1,
+      Name: "MohammadHijazi",
+      gender: "M",
+      inTeam: true,
+      desc: "TEchnology innovation Designer makers",
+      imgUrl:"../Innovation_files/images/image.jpg",
+      email:"mailto:piotr@mailtrap.io",
+      teams:"https://teams.microsoft.com/l/chat/0/0?users=m_hijazi@outlook.com"
     });
   },
   template: `
-    
-    <div class="row" class="usesList">
+<div class="row" class="usesList">
 
-        <div class="col-md-4"  v-for="user in users" :key="user.Id">
-            <div class="userItem" >
-         <div class="userpic"></div>
-                <div class="username">{{user.Name}}</div>
-                <div class="userdetails"><span class="gender">{{user.gender}}</span><span class="inteam">{{user.inTeam}}</span></div>
-                <div class="userBio">{{user.desc}}</div>
+    <div class="col-md-3" v-for="user in users" :key="user.Id">
+        <div class="userItem">
+            <div class="userpic"><img :src="user.imgUrl" /></div>
+            <div class="username">{{user.Name}}</div>
+            <div class="useractionLinks">
+                <ul>
+                    <li class="teamsLink"><a target="_blank" :href="user.teams">Teams</a></li>
+                    <li class="emailLink"><a target="_blank" :href="user.email">Email</a></li>
+                    <li class="sendLinkInvitation">Send Invitation</li>
+                    </ul>
             </div>
+            <div class="userdetails"><span class="gender">{{user.gender}}</span>
+                <span
+                    class="inteam">{{user.inTeam}}</span></div>
+            <div class="userBio">{{user.desc}}</div>
         </div>
     </div>
-    `,
+</div>
+`,
 });
