c00a1cb8ab5524dc75cf6a760839ca74f8826b5b
MHijazi <mohamadhijazi91@gmail.com>
2024-04-24 16:37:32 +0300

    footer

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,12 +1,11 @@
 // Register the `hello` component
 vueapp.component('FooterCompenent', {
     data: () => ({
-      name: 'World'
+      year: (new Date()).getFullYear()
     }),
     template: `
-      <div>
-        
-        <h4>Footer, {{name}}</h4>
+      <div>        
+        <h8>Copy rights @, {{year}}</h8>
       </div>
     `
   });
