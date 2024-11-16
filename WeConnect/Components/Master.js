cb5adc7b4363623d20dea78b79e5a7bf02a75d7f
MHijazi <mohamadhijazi91@gmail.com>
2024-11-16 20:01:42 +0300

    add QR generator

--- WeConnect/Components/Master.js
+++ WeConnect/Components/Master.js
@@ -6,8 +6,16 @@
  const {createVuetify} = Vuetify;
 
  const vuetify = createVuetify();
+
+ const {QrcodeCanvas, QrcodeSvg }=QrcodeVue;
+ //const QrcodeVue=QrcodeVue.default;
 //Vue.use(BootstrapVue);
+
  vueapp =createApp({
+  components: {
+    QrcodeCanvas: QrcodeVue.QrcodeCanvas,
+    QrcodeSvg: QrcodeVue.QrcodeSvg,
+  },
     // setup() {
     //   const title = ref('Introducing Amazon Echo Show 15: A smart display that blends into your home')
     //   return {
@@ -23,6 +31,11 @@
     <v-layout ref="app" class="rounded rounded-md">
     <HeaderComponenet></HeaderComponenet>
     <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+    </br>
+    <qrcode-canvas :value="'google.com'" level="H"></qrcode-canvas>
+    <qrcode-svg :value="'google.com'" level="H"></qrcode-svg>
+    
+    </br>
     <BodyComponent></BodyComponent>
     </v-main>
     <v-footer
