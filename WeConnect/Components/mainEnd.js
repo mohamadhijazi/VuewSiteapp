d452f3c6830d7a7b44080bc05206fe23e20241fe
MHijazi <mohamadhijazi91@gmail.com>
2024-11-16 22:08:02 +0300

    add PWA and js express

--- WeConnect/Components/mainEnd.js
+++ WeConnect/Components/mainEnd.js
@@ -1,3 +1,12 @@
 
 
-vueapp.use(vuetify).mount('#app')
+vueapp.use(vuetify).mount('#app');
+
+// window.onload = () => {
+//     'use strict';
+  
+//     if ('serviceWorker' in navigator) {
+//       navigator.serviceWorker
+//                .register('../innovation_files/sw.js');
+//     }
+//   }
---
+++ server.js
@@ -0,0 +1,16 @@
+const path = require('path')
+const express = require('express');
+
+const app = express();
+/*app.get('/', (req, res) => {
+    res.send('Successful response.');
+  });*/
+
+const publicDirPath = path.join(__dirname, '../VuewSiteapp')
+
+app.use(express.static(publicDirPath, {extensions: ['html']}))
+app.get('/', (req, res) => {
+  res.send(publicDirPath);
+});
+  
+app.listen(3000, () => console.log('Example app is listening on port 3000.'));
